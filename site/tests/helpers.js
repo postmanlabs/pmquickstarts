116fbfefe4a79bfd521fdb0545adfc3d03579c75
Gbadebo Bello <gbahdeybohbello@gmail.com>
2023-04-14 01:32:44 +0100

    Fixed failing workflow runs

--- site/tests/helpers.js
+++ site/tests/helpers.js
@@ -2,34 +2,22 @@ const fs = require('fs');
 const md = require('markdown-it')();
 
 
-// Recursively traverse through all the docs and convert all the markdowns to JSON
-const getTutorialsMetadata = async () => {
-    const tutorials = [];
-    
-    let tutorialsMarkdownFolderNames = fs.readdirSync('./pmguides/src');
 
-    // filter out folders to ignore
-    tutorialsMarkdownFolderNames = tutorialsMarkdownFolderNames.filter(folderName => {
-        const ignoredFolders = ['_imports', '_shared_assets', '_template', '.DS_Store'];
-        return !ignoredFolders.includes(folderName);
-    });
-    
-    for(let i = 1; i < tutorialsMarkdownFolderNames.length; i++){
-        const metadataObject = await tutorialMetadata(tutorialsMarkdownFolderNames[i]);
-        tutorials.push(metadataObject);
-    }
+const markDownToJson = (path) => {
+    const markdown = fs.readFileSync(path, 'utf-8');
+    const json = md.parse(markdown, { references: {} });
 
-    return tutorials;
+    return json;
 }
 
 const tutorialMetadata = (folderName) => new Promise(async (resolve) => {
-    let filesArray = await fs.promises.readdir(`./pmguides/src/${folderName}`)
+    let filesArray = await fs.promises.readdir(`./site/pmguides/src/${folderName}`)
 
     // filter out non markdown files
     let file = filesArray.filter(dirFile => dirFile.split('.').pop() === 'md');
 
     if(file.length > 0){
-        const path = `./pmguides/src/${folderName}/${file[0]}`;
+        const path = `./site/pmguides/src/${folderName}/${file[0]}`;
         const mdJson = markDownToJson(path);
         const metadataArray = mdJson[1].children;
 
@@ -49,12 +37,27 @@ const tutorialMetadata = (folderName) => new Promise(async (resolve) => {
     }
 })
 
-const markDownToJson = (path) => {
-    const markdown = fs.readFileSync(path, 'utf-8');
-    const json = md.parse(markdown, { references: {} });
+// Traverse through all the docs and convert all the markdowns to JSON
+const getTutorialsMetadata = async () => {
+    const tutorials = [];
+    
+    let tutorialsMarkdownFolderNames = fs.readdirSync('./site/pmguides/src');
 
-    return json;
+    // filter out folders to ignore
+    tutorialsMarkdownFolderNames = tutorialsMarkdownFolderNames.filter(folderName => {
+        const ignoredFolders = ['_imports', '_shared_assets', '_template', '.DS_Store'];
+        return !ignoredFolders.includes(folderName);
+    });
+    
+    for(let i = 1; i < tutorialsMarkdownFolderNames.length; i++){
+        const metadataObject = await tutorialMetadata(tutorialsMarkdownFolderNames[i]);
+        tutorials.push(metadataObject);
+    }
+
+    return tutorials;
 }
+
+getTutorialsMetadata();
 
 module.exports = { getTutorialsMetadata };
 
